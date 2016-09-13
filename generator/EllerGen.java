a2bc3c1333726db5ec53a60b2be599bea96f3e4e
jaalsh <jaalsh@users.noreply.github.com>
2016-09-13 09:40:36 +0100

    Animation Improvement

--- generator/EllerGen.java
+++ generator/EllerGen.java
@@ -4,7 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Random;
 
 import javax.swing.Timer;
@@ -18,25 +20,22 @@ import util.DisjointSets;
 
 public class EllerGen {
 	
-	private List<Cell> grid = new ArrayList<Cell>();
-	private List<Cell> currentCol = new ArrayList<Cell>();
+	private List<Cell> grid;
+	private List<Cell> currentCol;
 	
 	private DisjointSets disjointSet = new DisjointSets();
 	
 	private int colCounter = 0;
-	private int rows = Math.floorDiv(Maze.HEIGHT, Maze.W);
+	private int cols = Math.floorDiv(Maze.WIDTH, Maze.W);
 	private int fromIndex, toIndex;
 	
-	private Random r = new Random();
-	
-	private Cell current;
+	private boolean genNextCol = true;
 
 	public EllerGen(List<Cell> grid, MazeGridPanel panel) {
 		this.grid = grid;
-		current = grid.get(0);
 		
 		fromIndex = 0;
-		toIndex = rows;
+		toIndex = cols;
 		
 		for (int i = 0; i < grid.size(); i++) {
 			grid.get(i).setId(i);
@@ -47,58 +46,88 @@ public class EllerGen {
 		timer.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (!grid.parallelStream().allMatch(c -> c.isVisited())) {
-					carve();
-				} else {
-					current = null;
+				if (genNextCol) {
+					currentCol = grid.subList(fromIndex, toIndex);
+					colCounter++;
+					fromIndex = toIndex;
+					toIndex += cols;
+					new ColumnGen(currentCol, panel);
+				} else if (grid.parallelStream().allMatch(c -> c.isVisited())) {
 					Maze.generated = true;
 					timer.stop();
 				}
-				panel.setCurrent(current);
-				panel.repaint();
 			}
 		});
 		timer.start();
 	}
-
-	private void carve() {
-		currentCol = grid.subList(fromIndex, toIndex);
-		colCounter++;
-		fromIndex = toIndex;
-		toIndex += rows;
+	
+	private class ColumnGen {
 		
-		// carve down
-		for (Cell c : currentCol) {
-			c.setVisited(true);
-			current = c;
+		private Queue<Cell> carveDownQueue = new LinkedList<Cell>();
+		private Queue<Cell> carveRightQueue = new LinkedList<Cell>();
+		private List<Cell> col = new ArrayList<Cell>();
+		private Random r = new Random();
+		private Cell current;
+		
+		private ColumnGen(List<Cell> col, MazeGridPanel panel) {
+			genNextCol = false;
+			this.col = col;
+			carveDownQueue.addAll(col);
+			carveRightQueue.addAll(col);
 			
-			if (r.nextBoolean() || colCounter == Math.floorDiv(Maze.HEIGHT, Maze.W)) { // or last column
-				Cell bottom = c.getBottomNeighbour(grid);
+			final Timer timer = new Timer(Maze.speed, null);
+			timer.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (!carveDownQueue.isEmpty()) {
+						carveDown();
+					} else if (!carveRightQueue.isEmpty()) {
+						carveRight();
+					} else {
+						current = null;
+						genNextCol = true;
+						timer.stop();
+					}
+					panel.setCurrent(current);
+					panel.repaint();
+				}
+			});
+			timer.start();
+		}
+		
+		private void carveDown() {
+			current = carveDownQueue.poll();
+			current.setVisited(true);
+			
+			if (r.nextBoolean() || colCounter == cols) { // or last column
+				Cell bottom = current.getBottomNeighbour(grid);
 				if (bottom != null) {
-					if (disjointSet.find_set(c.getId()) != disjointSet.find_set(bottom.getId())) {
-						c.removeWalls(bottom);
-						disjointSet.union(c.getId(), bottom.getId());
+					if (disjointSet.find_set(current.getId()) != disjointSet.find_set(bottom.getId())) {
+						current.removeWalls(bottom);
+						disjointSet.union(current.getId(), bottom.getId());
 					}
 				}
 			}
-		}
+		}	
 		
-		// carve right
-		for (Cell c : currentCol) {
+		private void carveRight() {
+			Cell c = carveRightQueue.poll();
+			
 			List<Cell> cells = new ArrayList<Cell>();
-			for (Cell c2 : currentCol) {
+			for (Cell c2 : col) {
 				if (disjointSet.find_set(c.getId()) == disjointSet.find_set(c2.getId())) {
 					cells.add(c2);
 				}
 			}
 			Collections.shuffle(cells);
 			Cell c3 = cells.get(0);
-			current = c3;
 			Cell right = c3.getRightNeighbour(grid);
 			if (right != null) {
+				current = right;
+				right.setVisited(true);
 				c3.removeWalls(right);
 				disjointSet.union(c3.getId(), right.getId());
 			}
 		}
-	}	
+	}
 }
