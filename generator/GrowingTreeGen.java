e75905fe161eca601c302b77df1f9de9e9453fe3
jaalsh <jaalsh@users.noreply.github.com>
2016-09-20 18:31:57 +0100

    Comments

--- generator/GrowingTreeGen.java
+++ generator/GrowingTreeGen.java
@@ -13,11 +13,10 @@ import util.Cell;
 
 public class GrowingTreeGen {
 
-
-	private List<Cell> grid;
+	private final List<Cell> grid;
 	private Cell current;
-	private List<Cell> cells = new ArrayList<Cell>();
-	private Random r = new Random();
+	private final List<Cell> cells = new ArrayList<Cell>();
+	private final Random r = new Random();
 
 	public GrowingTreeGen(List<Cell> grid, MazeGridPanel panel) {
 		this.grid = grid;
@@ -41,31 +40,7 @@ public class GrowingTreeGen {
 		});
 		timer.start();
 	}
-	
-	// Growing tree algorithm: This is a general algorithm, capable of creating
-		// Mazes of different textures. It requires storage up to the size of the
-		// Maze. Each time you carve a cell, add that cell to a list. 
-	
-	//Proceed by  picking a cell from the list, and carving into an unmade cell next to it.
-	
-		// If there are no unmade cells next to the current cell, remove the current
-		// cell from the list. The Maze is done when the list becomes empty. The
-		// interesting part that allows many possible textures is how you pick a
-		// cell from the list. 
-	
-		//For example, if you always pick the most recent cell
-		// added to it, this algorithm turns into the recursive backtracker. If you
-		// always pick cells at random, this will behave similarly but not exactly
-		// to Prim's algorithm. If you always pick the oldest cells added to the
-		// list, this will create Mazes with about as low a "river" factor as
-		// possible, even lower than Prim's algorithm. If you usually pick the most
-		// recent cell, but occasionally pick a random cell, the Maze will have a
-		// high "river" factor but a short direct solution. If you randomly pick
-		// among the most recent cells, the Maze will have a low "river" factor but
-		// a long windy solution.
 
-	
-	// low river factor by attempting to pick oldest cell if possible. else get random.
 	private void carve() {
 		current.setVisited(true);
 		Cell next = current.getUnvisitedNeighbour(grid);
@@ -76,11 +51,7 @@ public class GrowingTreeGen {
 		} else {
 			cells.remove(current);
 			if (!cells.isEmpty()) {
-				if (cells.get(0).getUnvisitedNeighboursList(grid).size() != 0) {
-					current = cells.get(0);
-				} else {
-					current = cells.get(r.nextInt(cells.size()));
-				}
+				current = cells.get(r.nextInt(cells.size())); // get random cell
 			}
 		}
 	}
